3c49ad6cf36091ec75d98f58930dc73f2acd3462
liusi <liusi@thinkxe.com>
2020-10-14 19:51:00 +0800

    it

---
+++ aes/AesEncryptor.h
@@ -0,0 +1,34 @@
+//
+// Created by liusi on 10/13/20.
+//
+
+#ifndef AI_TOOLCHAIN_AESENCRYPTOR_H
+#define AI_TOOLCHAIN_AESENCRYPTOR_H
+
+#include <string>
+
+class AES;
+
+class AesEncryptor
+{
+public:
+    AesEncryptor(unsigned char* key);
+    ~AesEncryptor(void);
+
+    std::string EncryptString(std::string strInfor);
+    std::string DecryptString(std::string strMessage);
+
+    void EncryptTxtFile(const char* inputFileName, const char* outputFileName);
+    void DecryptTxtFile(const char* inputFileName, const char* outputFileName);
+
+private:
+    void Byte2Hex(const unsigned char* src, int len, char* dest);
+    void Hex2Byte(const char* src, int len, unsigned char* dest);
+    int  Char2Int(char c);
+
+private:
+    AES* m_pEncryptor;
+};
+
+
+#endif //AI_TOOLCHAIN_AESENCRYPTOR_H
